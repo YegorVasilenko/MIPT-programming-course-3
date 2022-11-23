ce502614f6a3ef60a2120c8155131ebdc92d5b2c
Yegor Vasilenko <113385271+YegorVasilenko@users.noreply.github.com>
2022-11-24 00:21:51 +0300

    Update List.py

--- seminar 4/List.py
+++ seminar 4/List.py
@@ -1,131 +1,203 @@
-class Node:
-    def __init__(self, value,
-                 prev_pointer=None, next_pointer=None):
-        self.set_value(value)
-        self.set_prev(prev_pointer)
-        self.set_next(next_pointer)
+import math
 
-    def get_value(self):
-        return self._value
 
-    def get_next(self):
-        return self._next_pointer
+# краткая справка по классу (будет дополняться)
+# параметры
+# _Re | вещественная часть алгебраической формы
+# _Im | мнимая часть алгебраической формы
+# _r  | модуль тригонометрической формы
+# _phi| аргумент тригонометрической формы
+# методы
+# sum |
+# substract |
+# product |
+# divide |
+# get_exponential_form |
+# get_algebraic_form |
 
-    def get_prev(self):
-        return self._prev_pointer
 
-    def set_value(self, value):
-        self._value = value
+class ComplexNumber:
 
-    def set_prev(self, prev_pointer):
-        self._prev_pointer = prev_pointer
 
-    def set_next(self, next_pointer):
-        self._next_pointer = next_pointer
+    """
+    организация класса
+    """
+
+
+    # инициализация проводится в алгебраической форме
+    def __init__(self, Re, Im):
+        self._Re = Re
+        self._Im = Im
+        self._r, self._phi = self.get_exponential_form()
+
+
+    # простая доморощенная функция для отладки
+    def error(self, msg):
+        print(msg)
+        exit(-1)
+
+
+    """
+    простые геттеры
+    """
+
+
+    def get_Re(self):
+        return self._Re
+
+
+    def get_Im(self):
+        return self._Im
+
+    def get_r(self):
+        return self._r
+
+
+    def get_phi(self):
+        return self._phi
+
+
+    """
+    сеттеры, учитывающие все обновления
+    """
+
+
+    def set_Re(self, Re):
+        self._Re = Re
+        self._r, self._phi = self.get_exponential_form()
+
+
+    def set_Im(self, Im):
+        self._Im = Im
+        self._r, self._phi = self.get_exponential_form()
+
+
+    def set_r(self, r):
+        self._r = r
+        self._Re, self._Im = self.get_algebraic_form()
+
+
+    def set_phi(self, phi):
+        self._phi = phi
+        self._Re, self._Im = self.get_algebraic_form()
+
+
+    """
+    переводы между алгебраической и тригонометрической формами
+    """
+
+
+    # из алгебраической формы вычисляет и возвращает
+    # модуль и аргумент
+    def get_exponential_form(self):
+        r = math.sqrt(self._Re**2 + self._Im**2)
+        x = self._Re
+        y = self._Im
+
+        if x > 0 and y == 0:
+            phi = 0
+        elif x > 0 and y > 0:
+            phi = math.atan(abs(y/x))
+        elif x == 0 and y > 0:
+            phi = math.pi / 2
+        elif x < 0 and y > 0:
+            phi = math.pi - math.atan(abs(y/x))
+        elif x < 0 and y == 0:
+            phi = math.pi
+        elif x < 0 and y < 0:
+            phi = -math.pi + math.atan(abs(y/x))
+        elif x == 0 and y < 0:
+            phi = - math.pi / 2
+        elif x > 0 and y < 0:
+            phi = -math.atan(abs(y/x))
+
+        return r, phi
+
+
+    # из экспоненциальной формы вычисляет и возвращает
+    # Re и Im части
+    def get_algebraic_form(self):
+        Re = self._r * math.cos(self._phi)
+        Im = self._r * math.sin(self._phi)
+        return Re, Im
+
+
+    """
+    методы-реализации арифметических операций
+    """
+
+
+    # возвращает новое комплексное число -- сумму данного числа и аргумента
+    def sum(self, z):
+        return ComplexNumber(self.get_Re() + z.get_Re(), self.get_Im() + z.get_Im())
+
+
+    # возвращает новое комплексное число -- разность данного числа и аргумента,
+    # аргумент вычитается из данного числа
+    def substract(self, z):
+        return ComplexNumber(self.get_Re() - z.get_Re(), self.get_Im() - z.get_Im())
+
+
+    # возвращает новое комплексное число -- произведение данного числа и аргумента
+    def product(self, z):
+        return ComplexNumber(self.get_Re() * z.get_Re() - self.get_Im() * z.get_Im(),
+                             self.get_Re() * z.get_Im() + self.get_Im() * z.get_Re())
+
+
+    # возвращает новое комплексное число -- частное данного числа и аргумента,
+    # аргумент вычитается из данного числа
+    def divide(self, z):
+        if z.get_Re() == 0 and z.get_Im() == 0:
+            error("division by zero is impossible")
+        a = self.get_Re()
+        b = self.get_Im()
+        c = z.get_Re()
+        d = z.get_Im()
+        return ComplexNumber((a*c+b*d)/(c**2+d**2), (b*c-a*d)/(c**2+d**2))
+
+
+    """
+    перегрузка операторов
+    """
+
+
+    def __add__(self, z):
+        return self.sum(z)
+
+
+    def __sub__(self, z):
+        return self.substract(z)
+
+
+    def __truediv__(self, z):
+        return self.divide(z)
+
+
+    def __mul__(self, z):
+        return self.product(z)
+
+
+    def __eq__(self, z):
+        return self.get_Re() == z.get_Re() and self.get_Im() == z.get_Im()
+
 
     def __str__(self):
-        return str(self.get_value())
+        return str(self.get_Re()) + " + i * " + str(self.get_Im())
 
 
-class List:
-    def __init__(self, collection = None):
-        self._start_pointer = None
-        self._finish_pointer = None
-        self._length = 0
+    def __abs__(self):
+        return (self.get_Re()**2 + self.get_Im()**2)**0.5
 
-        # initialization with some collection structure
-        if collection != None:
-            for el in collection:
-                self.append(el)
-
-    def __len__(self):
-        return self._length
-
-    def append(self, value):
-        if self._length == 0:
-            self._start_pointer = Node(value)
-            self._finish_pointer = self._start_pointer
-            self._length = 1
-        else:
-            self._finish_pointer.set_next(Node(value,
-                                               self._finish_pointer))
-            self._finish_pointer = self._finish_pointer.get_next()
-            self._length += 1
 
     def __getitem__(self, i):
-        if i < 0 or i >= self._length:
-            return False
-
-        # select an element using
-        # not more than ceil(N / 2) operations
-        if i <= self._length // 2:
-            curr_pointer = self._start_pointer
-            for j in range(i):
-                curr_pointer = curr_pointer.get_next()
-        else:
-            curr_pointer = self._finish_pointer
-            for j in range(self._length - i - 1):
-                curr_pointer = curr_pointer.get_prev()
-        return curr_pointer.get_value()
-
-    def __str__(self):
-        arr = []
-        for i in range(self._length):
-            arr.append(str(self[i]))
-        return "[" + ", ".join(arr) + "]"
-
-    # addition operator overloaded for my lists
-    def __add__(self, L):
-        res = List()
-        for i in range(len(self)):
-            res.append(self[i])
-        for i in range(len(L)):
-            res.append(L[i])
-        return res
-
-    # remove an element from the list
-    def pop(self, i):
-        if i < 0 or i >= self._length:
-            return False
-
-        self._length -= 1
-
         if i == 0:
-            self._start_pointer = self._start_pointer.get_next()
-            self._start_pointer.set_prev(None)
-            return True
+            return self.get_Re()
+        if i == 1:
+            return self.get_Im()
+        return None
 
-        if i == self._length: # not length - 1 because it was decreased
-            self._finish_pointer = self._finish_pointer.get_prev()
-            self._finish_pointer.set_next(None)
-            return True
 
-        if i <= self._length // 2:
-            curr_pointer = self._start_pointer
-            for j in range(i):
-                curr_pointer = curr_pointer.get_next()
-        else:
-            curr_pointer = self._finish_pointer
-            for j in range(self._length - i - 1):
-                curr_pointer = curr_pointer.get_prev()
-
-        curr_pointer.get_prev().set_next(curr_pointer.get_next())
-
-        return True
-
-A = List((1, 2, 3, 4, 5))
-B = List([6, 7, 8, 9, 10])
-print(A)
-print(B)
-S = A + B
-print(S)
-print(S[0])
-print(S[1])
-print(S[8])
-print(S[9])
-S.pop(0)
-print(S)
-S.pop(8)
-print(S)
-S.pop(3)
-print(S)
+z = ComplexNumber(3, 4)
+print(z)
+print(abs(z))
+print(z[0], z[1])
